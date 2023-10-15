a2c8a4c60009309d1fa91e516bc42a9c0b010577
tushar <tushartripathi301997@gmail.com>
2023-10-15 23:41:27 +0530

    bug fix

--- app/src/main/java/com/example/webbookmarker/ui/TakeNote/MainActivity.kt
+++ app/src/main/java/com/example/webbookmarker/ui/TakeNote/MainActivity.kt
@@ -59,13 +59,11 @@ class MainActivity : AppCompatActivity() {
         getIntentValeus()
         val webSettings: WebSettings = binding.mainWebViewId.getSettings()
         webSettings.javaScriptEnabled = true
-        setUrl()
+        //setUrl()
 
         viewModelOperations()
 
         binding.mainWebViewId.setOnTouchListener { _, event ->
-            Log.d("asdfasdasdf","event foudn y = ${event.y}")
-            Log.d("asdfasdasdf","event foudn raw y = ${event.rawY}")
 
             yAxisPosition = event.rawY.toInt() // Y-coordinate of the touch event
             Toast.makeText(this,yAxisPosition,Toast.LENGTH_SHORT).show()
@@ -89,20 +87,15 @@ class MainActivity : AppCompatActivity() {
             webViewClient = object : WebViewClient(){
                 override fun onPageFinished(view: WebView?, url: String?) {
                     super.onPageFinished(view, url)
-
                     runBlocking {
                         getViewModelData()
                     }
-
                 }
             }
             setWebContentsDebuggingEnabled(true)
-
             addJavascriptInterface(LongPressJavaScriptInterface(), "Android")
-
         }
         url?.let { binding.mainWebViewId.loadUrl(it) }
-
     }
 
     private fun getIntentValeus() {
